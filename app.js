7174906fe29afc21fac4fd082676d8a52a2e16d3
Tobias <70690455+Tobias@users.noreply.github.com>
2023-01-21 18:37:11 +0100

    fgf
    fgfg

--- app.js
+++ app.js
@@ -61,6 +61,8 @@ let isUpdate;
 //---------------------------------------
 const app = express();
 
+let cookieSession = require('cookie-session')
+let cookieParser = require("cookie-parser")
 app.set('view engine', 'ejs');
 
 app.use(paginate.middleware(10, 50))
@@ -70,8 +72,9 @@ app.use(express.static(path.join(__dirname, "/uploads")));
 app.use(express.static(path.join(__dirname + "/public")));
 app.use(express.static(path.join(__dirname, "/public/javascript")))
 app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+//app.use(cookieParser());
 app.use(session({
-    secret: process.env.SECRET,
+    secret: process.env["SESSION-SECRET"],
     resave:false,
     saveUnitialized: true,
 }));
